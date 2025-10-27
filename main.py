1dfe5372f09a77f237f603929c180535ecb101d3
Patel Rudrakumar <hitendra22051914@gmail.com>
2025-10-28 00:33:29 +0530

    Fixed login bug and updated README

--- main.py
+++ main.py
@@ -1,48 +1,216 @@
-from  fastapi import FastAPI, HTTPException
-from   pydantic import BaseModel ,Field
-from uuid import UUID
+from fastapi import FastAPI, status
+from fastapi.exceptions import HTTPException
+from pydantic import BaseModel
+from typing import List
+books = [
+    {
+        "id": 1,
+        "title": "Think Python",
+        "author": "Allen B. Downey",
+        "publisher": "O'Reilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 2,
+        "title": "Django By Example",
+        "author": "Antonio Mele",
+        "publisher": "Packt Publishing Ltd",
+        "published_date": "2022-01-19",
+        "page_count": 1023,
+        "language": "English",
+    },
+    {
+        "id": 3,
+        "title": "The web socket handbook",
+        "author": "Alex Diaconu",
+        "publisher": "Xinyu Wang",
+        "published_date": "2021-01-01",
+        "page_count": 3677,
+        "language": "English",
+    },
+    {
+        "id": 4,
+        "title": "Head first Javascript",
+        "author": "Hellen Smith",
+        "publisher": "Oreilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 540,
+        "language": "English",
+    },
+    {
+        "id": 5,
+        "title": "Algorithms and Data Structures In Python",
+        "author": "Kent Lee",
+        "publisher": "Springer, Inc",
+        "published_date": "2021-01-01",
+        "page_count": 9282,
+        "language": "English",
+    },
+    {
+        "id": 6,
+        "title": "Head First HTML5 Programming",
+        "author": "Eric T Freeman",
+        "publisher": "O'Reilly Media",
+        "published_date": "2011-21-01",
+        "page_count": 3006,
+        "language": "English",
+    },
+]
+
+
 
 
 app = FastAPI()
+
 class Book(BaseModel):
-    id: UUID
-    title: str = Field(min_length=1)
-    author: str = Field(min_length=1, max_length=100)
-    description: str | None = Field(default=None, min_length=1, max_length=100)
-    rating: int = Field(gt=-1, lt=101)
+    id: int
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
+
+class BookUpdateModel(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    page_count: int
+    language: str
 
 
-Books = []
-@app.get("/greet/{name}")
-def read_root(name: str, age: int):
-    return {"Diya": f"{name}", "age": f"{age}"}
-
-@app.get("/")
-def read_api():
-    return {"Books": Books}
-
-@app.post("/")
-def create_book(book: Book):
-    Books.append(book)
-    return book
+@app.get("/books", response_model=List[Book])
+async def get_all_books():
+    return books
 
 
-@app.put("/{book_id}")
-def update_book(book_id: UUID, book: Book):
-    counter = 0
-    for b in Books:
-        if b.id == book_id:
-            Books[counter-1] = book
-            return Books[counter-1]
-        raise HTTPException(status_code=404, detail="Book not found")
-        counter += 1
-    raise HTTPException(status_code=404, detail="Book not found")
+@app.post("/books", status_code=status.HTTP_201_CREATED)
+async def create_a_book(book_data: Book) -> dict:
+    new_book = book_data.model_dump()
+
+    books.append(new_book)
+
+    return new_book
 
 
-@app.delete("/{book_id}")
-def delete_book(book_id: UUID):
-    for index, b in enumerate(Books):
-        if b.id == book_id:
-            del Books[index]
-            return {"Message": "Book deleted successfully"}
-    raise HTTPException(status_code=404, detail="Book not found")
+@app.get("/book/{book_id}")
+async def get_book(book_id: int) -> dict:
+    for book in books:
+        if book["id"] == book_id:
+            return book
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
+
+@app.patch("/book/{book_id}")
+async def update_book(book_id: int,book_update_data:BookUpdateModel) -> dict:
+    for book in books:
+        if book['id'] == book_id:
+            book['title'] = book_update_data.title
+            book['publisher'] = book_update_data.publisher
+            book['page_count'] = book_update_data.page_count
+            book['language'] = book_update_data.language
+            return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+@app.delete("/book/{book_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            books.remove(book)
+            return {}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from  fastapi import FastAPI, HTTPException
+# from   pydantic import BaseModel ,Field
+# from uuid import UUID
+
+
+# app = FastAPI()
+# class Book(BaseModel):
+#     id: UUID
+#     title: str = Field(min_length=1)
+#     author: str = Field(min_length=1, max_length=100)
+#     description: str | None = Field(default=None, min_length=1, max_length=100)
+#     rating: int = Field(gt=-1, lt=101)
+
+
+# Books = []
+# @app.get("/greet/{name}")
+# def read_root(name: str, age: int):
+#     return {"Diya": f"{name}", "age": f"{age}"}
+
+# @app.get("/")
+# def read_api():
+#     return {"Books": Books}
+
+# @app.post("/")
+# def create_book(book: Book):
+#     Books.append(book)
+#     return book
+
+
+# @app.put("/{book_id}")
+# def update_book(book_id: UUID, book: Book):
+#     counter = 0
+#     for b in Books:
+#         if b.id == book_id:
+#             Books[counter-1] = book
+#             return Books[counter-1]
+#         raise HTTPException(status_code=404, detail="Book not found")
+#         counter += 1
+#     raise HTTPException(status_code=404, detail="Book not found")
+
+
+# @app.delete("/{book_id}")
+# def delete_book(book_id: UUID):
+#     for index, b in enumerate(Books):
+#         if b.id == book_id:
+#             del Books[index]
+#             return {"Message": "Book deleted successfully"}
+#     raise HTTPException(status_code=404, detail="Book not found")
+
