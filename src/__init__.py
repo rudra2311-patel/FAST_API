f35b059534ece7a62f3a068d95ff720a208ad4cf
Patel Rudrakumar <hitendra22051914@gmail.com>
2025-11-06 20:50:52 +0530

    added auth

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from src.books.routes import book_router
 from contextlib import asynccontextmanager
 from src.db.main import init_db
+from src.auth.routes import auth_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -21,3 +22,4 @@ app = FastAPI(
 )
 
 app.include_router(book_router, prefix=f"/api{version1}/books", tags=["books"])
+app.include_router(auth_router, prefix=f"/api{version1}/auth", tags=["auth"])
