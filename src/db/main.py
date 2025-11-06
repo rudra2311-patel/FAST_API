13e460793811836a9b04ecdd94e5cd585fa37caa
Patel Rudrakumar <hitendra22051914@gmail.com>
2025-11-06 12:03:07 +0530

    updated the main

--- src/db/main.py
+++ src/db/main.py
@@ -1,17 +1,22 @@
-from sqlmodel  import  create_engine,text
+from sqlmodel import create_engine, SQLModel,text
 from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker as sessionmaker
 from src.config import Config
-from src.books.model import Book
-from sqlmodel import SQLModel
-
-
-engine =  AsyncEngine(
-    create_engine(
+async_engine = AsyncEngine (create_engine(
     url=Config.DATABASE_URL,
     echo=True,
 ))
 async def init_db():
-    async with engine.begin() as conn:
-       from src.books.model import Book
-       await conn.run_sync(SQLModel.metadata.create_all)
-      
+    async with async_engine.begin() as conn:
+        from src.books.model import Book  
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session() -> AsyncSession:
+    Session = sessionmaker(
+        bind=async_engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+    )
+    async with Session() as session:
+        yield session
