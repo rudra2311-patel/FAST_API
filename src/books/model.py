21bbc591990b56b2dd865a0bd988e4c2d7d567d2
Patel Rudrakumar <hitendra22051914@gmail.com>
2025-11-06 12:01:38 +0530

    updated the model

--- src/books/model.py
+++ src/books/model.py
@@ -1,30 +1,32 @@
-
-from sqlmodel import SQLModel,Field,Column
-from datetime import datetime
+from sqlmodel import SQLModel, Field, Column
+from datetime import datetime, date
 import sqlalchemy.dialects.postgresql as pg
 import uuid
 
 
 class Book(SQLModel, table=True):
     __tablename__ = "books"
-    id: uuid.UUID = Field(
-        sa_column= Column(
-            pg.UUID(as_uuid=True),  
-            nullable=False,
-            primary_key=True,
-            default=uuid.uuid4
-        )
 
+    id: uuid.UUID = Field(
+        default_factory=uuid.uuid4,
+        sa_column=Column(pg.UUID(as_uuid=True), primary_key=True, nullable=False),
     )
+
     title: str
     author: str
     publisher: str
-    published_date: str
+    published_date: date
     page_count: int
     language: str
-    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP,default=datetime.now()))
-    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP,default=datetime.now()))
 
-def __repr__(self) -> str:
+    created_at: datetime = Field(
+        default_factory=datetime.now,
+        sa_column=Column(pg.TIMESTAMP, nullable=False)
+    )
+    updated_at: datetime = Field(
+        default_factory=datetime.now,
+        sa_column=Column(pg.TIMESTAMP, nullable=False)
+    )
+    
+    def __repr__(self) -> str:
         return f"Book(id={self.id}, title={self.title}, author={self.author})"
-
